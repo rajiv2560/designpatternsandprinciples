e741c4701d2c30f1cd8f0c56a676f0c1ae09b7a1
Rajiv Shrestha <rajiv2560shrestha@gmail.com>
2017-06-06 14:45:01 +1000

    Added TypeValidator and few modifications to code base.

--- OCP-Follow/Program.cs
+++ OCP-Follow/Program.cs
@@ -8,8 +8,34 @@ namespace OCP_Follow
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var sourceServerData = new List<SourceServerData>();
+            var serverData = new List<ServerData>();
+
+            foreach (var data in serverData)
+            {
+                var sourceData = sourceServerData.FirstOrDefault(s => s.RecordIdentifier == data.RecordIdentifier);
+                var isValid = IsValid(data, sourceData);
+                Console.WriteLine("Record with Id {0} is {1}", data.RecordIdentifier, isValid);
+                Console.ReadLine();
+            }
+        }
+
+        private static object IsValid(ServerData data, SourceServerData sourcedData)
+        {
+            List<IValidator> validators = AddValidatorsToValidate();
+            IValidateData validateData = new ValidateData(validators);
+            return validateData.Validate(data, sourcedData);
+        }
+
+        private static List<IValidator> AddValidatorsToValidate()
+        {
+            return new List<IValidator>
+            {
+                new IpValidator(),
+                new TypeValidator()
+            };
         }
     }
 }
---
+++ OCP-Follow/TypeValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_Follow
+{
+    public class TypeValidator : IValidator
+    {
+        public bool IsValid(ServerData data, SourceServerData sourceData)
+        {
+            return data.Type == sourceData.Type;
+        }
+    }
+}
